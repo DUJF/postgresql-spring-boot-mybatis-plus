e3df57566dd3056f04268c051bb01a88ffe0d8d0
jeffrey <31514047+DUJF@users.noreply.github.com>
2018-05-17 17:18:09 +0800

    Add files via upload

---
+++ src/main/java/com/github/web/service/RoleService.java
@@ -0,0 +1,16 @@
+package com.github.web.service;
+
+import com.github.web.model.Role;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ * ${table.comment} 服务类
+ * </p>
+ *
+ * @author dujf
+ * @since 2018-05-17
+ */
+public interface RoleService extends IService<Role> {
+	
+}
